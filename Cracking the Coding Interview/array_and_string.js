6f12414cab278cbd1347d5c661bc554bbe6c5e9f
tnizam <tahmina.nizam94@gmail.com>
2021-02-06 21:04:06 -0500

    is unique js problem

--- Cracking the Coding Interview/array_and_string.js
+++ Cracking the Coding Interview/array_and_string.js
@@ -1,1 +1,8 @@
 // Chapter 1: Array and string problems
+
+// Is Unique: Implement an algorithm to determine if a string has all unique characters. 
+// What if you cannot use additional data structures?
+
+function isUnique() {
+    
+} 
