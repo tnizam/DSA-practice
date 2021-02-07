4c84d24f27a6ef076c389195530c572d595eab99
tnizam <tahmina.nizam94@gmail.com>
2021-02-07 00:53:46 -0500

    is unique ruby

---
+++ Cracking the Coding Interview/array_and_string.rb
@@ -0,0 +1,12 @@
+=begin
+ Chapter 1: Array and string problems
+
+ Is Unique: Implement an algorithm to determine if a string has all unique characters. 
+ What if you cannot use additional data structures?
+
+=end
+
+def is_unique(str) 
+    char_hash = {}
+    
+end
