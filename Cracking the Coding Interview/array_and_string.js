f9ff008d045b4a795cf0208e3e6a3cdad5fd92d9
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-02-18 20:11:57 -0500

    Palindrome Permutation js

--- Cracking the Coding Interview/array_and_string.js
+++ Cracking the Coding Interview/array_and_string.js
@@ -155,3 +155,14 @@ function URLify(str, num) {
 }
 
 console.log(URLify("hel lo", 5))
+
+// ------------------------------------------------------------------------------
+
+// Palindrome Permutation: Given a string, write a function to check if it is a permutation 
+// of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation
+// is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words
+
+// EXAMPLE
+// Input: Tact Coa
+// Output: True (permutations: "taco cat", "atco eta", etc.)
+
