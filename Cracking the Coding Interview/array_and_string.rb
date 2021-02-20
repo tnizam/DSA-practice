f88647dbd17f16370b5ae0eaf43fb1430969141c
tnizam <tahmina.nizam94@gmail.com>
2021-02-20 00:37:17 -0500

    Check Permutation ruby solved

--- Cracking the Coding Interview/array_and_string.rb
+++ Cracking the Coding Interview/array_and_string.rb
@@ -20,4 +20,48 @@ def is_unique(str)
     return true
 end
 
-puts(is_unique("stre"))
+# puts(is_unique("stre"))
+
+# //-----------------------------------------------------------------------------
+
+# // Check Permutation: Given two strings, write a method to decide 
+# // if one is a permutation of the other. 
+
+# // note: same letters but different order
+
+def check_perm(str1, str2)
+    if str1.length != str2.length
+        return false
+    end
+
+    hash1 = Hash.new(0)
+    hash2 = Hash.new(0)
+
+    str1.each_char do |char|
+        hash1[char] += 1
+    end
+
+    str2.each_char do |char|
+        hash2[char] += 1
+    end
+
+    hash1.each do |key, value| 
+        if(hash1[key] != hash2[key])
+            return false
+        end
+    end
+
+    return true;
+end
+
+# puts(check_perm("hello", "elwoh"))
+
+# // URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string. (Note: If implementing in Java, please use a character array so that you can perform this operation in place.) 
+
+# // replace all spaces with '%20' 
+# // the true length of the string is given
+# // subtract chars in the string without space from true length to see how many spaces we are allowed to replace
+
+def URLify
+    
+end
