f866221e48a5b7f2979b9716bcf42f4cb5d01d75
tnizam <tahmina.nizam94@gmail.com>
2021-02-21 00:40:06 -0500

    urlify rb problem

--- Cracking the Coding Interview/array_and_string.rb
+++ Cracking the Coding Interview/array_and_string.rb
@@ -56,6 +56,8 @@ end
 
 # puts(check_perm("hello", "elwoh"))
 
+# ---------------------------------------------------------------------------
+
 # // URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string. (Note: If implementing in Java, please use a character array so that you can perform this operation in place.) 
 
 # // replace all spaces with '%20' 
@@ -63,5 +65,5 @@ end
 # // subtract chars in the string without space from true length to see how many spaces we are allowed to replace
 
 def URLify
-    
+
 end
