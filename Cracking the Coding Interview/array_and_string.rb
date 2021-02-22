ca0c228e4828d39ebb21114cef0ddc1fe7979a1b
tnizam <tahmina.nizam94@gmail.com>
2021-02-22 11:39:16 -0500

    One Way rb problem

--- Cracking the Coding Interview/array_and_string.rb
+++ Cracking the Coding Interview/array_and_string.rb
@@ -91,4 +91,15 @@ def URLify(str, num)
     return new_str
 end
 
-puts(URLify("hel lo", 6))
+# puts(URLify("hel lo", 6))
+
+# // -----------------------------------------------------------------------
+
+# // One Away: There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away. 
+
+# // EXAMPLE
+# // pale, ple -> true
+# // pales, pale -> true
+# // pale, bale -> true
+# // pale, bake -> false
+
