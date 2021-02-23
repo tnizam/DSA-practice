554f5bc2b3df49e34a468f9b6216d3b03e1036d6
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-02-23 13:48:44 -0500

    remove dup js solved

--- Cracking the Coding Interview/linked_lists.js
+++ Cracking the Coding Interview/linked_lists.js
@@ -6,8 +6,39 @@
     Notes:
     make a set recording the numbers in the set if we want it to be O(n)
         therefore making a hashmap adding the numbers we have on the linked list
+    - move the next distinct pointer to the next node
+    - deleting the previous node
+    - change the current node to the next distinct node
+    - keep checking down the line to see the next distinct node that is not the same
 */
 
-function removeDups() {
+class LinkedList {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
 
+function removeDups(linkedList) {
+    let dupSet = new Set();
+    let currentNode = linkedList.head;
+    let prevNode = null;
+
+    while(currentNode) {
+        if(dupSet.has(currentNode.value)) {
+            // if it has the value
+            // inorder to ensure skipping that node we will set prev.next to curr.next
+            // as well as setting curr to curr.next
+            // this will skip to next
+
+            prevNode.next = currentNode.next;
+            currentNode = currentNode.next;
+        } else {
+            dupSet.add(currentNode.value);
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+        }
+    }
+
+    return linkedList;
 }
