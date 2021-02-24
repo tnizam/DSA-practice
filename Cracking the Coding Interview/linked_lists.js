1b884f936ef73f4c7bb237218f76cbd5ef4701a6
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-02-23 23:34:53 -0500

    Remove Dups js prob

---
+++ Cracking the Coding Interview/linked_lists.js
@@ -0,0 +1,13 @@
+/*
+2.1 Remove Dups! Write code to remove duplicates from an unsorted linked list.
+    FOLLOW UP
+    How would you solve this problem if a temporary buffer is not allowed?
+
+    Notes:
+    make a set recording the numbers in the set if we want it to be O(n)
+        therefore making a hashmap adding the numbers we have on the linked list
+*/
+
+function removeDups() {
+
+}
