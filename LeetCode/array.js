236bb05a4dc8e30bc5436883971759d5c3e9ea76
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-06 15:51:50 -0500

    array leetcode

---
+++ LeetCode/array.js
@@ -0,0 +1,23 @@
+/*
+Running Sum of 1d Array
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+*/
+
+/**
+ notes:
+    keep the first number in the array and keep adding down the line
+ */
+var runningSum = function(nums) {
+    let sums = [];
+    let lastNum = 0;
+    
+    for(let i = 0; i < nums.length; i++) {
+        lastNum = lastNum + nums[i];
+        sums.push(lastNum);
+    }
+    
+    return sums
+};
