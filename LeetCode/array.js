8e47d1c0d6c750c987b16badd8f1e4e683b3754f
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-06 16:07:18 -0500

    update

--- LeetCode/array.js
+++ LeetCode/array.js
@@ -11,7 +11,6 @@ Return the running sum of nums.
     keep the first number in the array and keep adding down the line
  */
 var runningSum = function(nums) {
-    let sums = {};
     let lastNum = 0;
     
     for(let i = 0; i < nums.length; i++) {
@@ -19,6 +18,6 @@ var runningSum = function(nums) {
         sums[i] = lastNum;
     }
     
-    let sumArr = Object.values(sum);
+    let sumArr = Object.values(sums);
     return sumArr;
 };
