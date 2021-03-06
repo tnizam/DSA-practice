d9848c0f947ec95fd59502ab3a7526cc1084777d
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-05 20:18:46 -0500

    algo exp stacks

---
+++ Algo Expert/Stack.js
@@ -0,0 +1,28 @@
+//Min Max Stack Construction
+
+class MinMaxStack {
+	constructor() {
+		this.stack = [];
+		
+	}
+  peek() {
+    // Write your code here.
+  }
+
+  pop() {
+    // Write your code here.
+  }
+
+  push(number) {
+    // Write your code here.
+  }
+
+  getMin() {
+    // Write your code here.
+  }
+
+  getMax() {
+    // Write your code here.
+  }
+}
+
