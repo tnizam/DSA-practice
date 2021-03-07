b8dc44f09d9e3d4a7a1ecf0846202f39f6916611
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-07 16:56:32 -0500

    max wealth problem update

--- LeetCode/array.js
+++ LeetCode/array.js
@@ -34,7 +34,10 @@ var maximumWealth = function(accounts) {
     let max = 0;
     
     for(let i = 0; i < accounts.length; i++) {
-        let sum = accounts[i].reduce((acc, n) => acc + n);
+        let sum = 0;
+        for(let j = 0; j < accounts[i].length; j++) {
+            sum += accounts[i][j];
+        }
         if(sum >= max) {
             max = sum;
         }
