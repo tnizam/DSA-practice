a6064adb6a1ba47b04b4b6e7c6b88743e3e18c65
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-08 18:18:14 -0500

    Kids With the Greatest Number of Candies problem js

--- LeetCode/array.js
+++ LeetCode/array.js
@@ -47,3 +47,12 @@ var maximumWealth = function(accounts) {
 };
 
 console.log(maximumWealth([[1,5],[7,3],[3,5]]))
+
+/*
+Kids With the Greatest Number of Candies
+
+Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+
+For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
+
+*/
