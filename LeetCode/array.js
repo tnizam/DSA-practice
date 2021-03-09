54375bfecd878047d8f1ae7c07fa4793872ed58d
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-08 23:12:25 -0500

    Shuffle the Array problem

--- LeetCode/array.js
+++ LeetCode/array.js
@@ -79,3 +79,12 @@ var kidsWithCandies = function(candies, extraCandies) {
     let arr = Object.values(isGreater);
     return arr;
 };
+
+/*
+Shuffle the Array
+
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+*/
+
