61dd46543995eeb056ea4dd7054067982f5ef5f7
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-10 23:33:37 -0500

    good pairs solved

--- LeetCode/array.js
+++ LeetCode/array.js
@@ -108,3 +108,19 @@ A pair (i,j) is called good if nums[i] == nums[j] and i < j.
 
 Return the number of good pairs.
 */
+
+var numIdenticalPairs = function(nums) {
+    let good = 0;
+    
+    for(let i = 0; i < nums.length; i++) {
+        
+        for(let j = i+1; j < nums.length; j++) {
+            
+            if(nums[i] === nums[j]) {
+                good += 1;
+            }
+        }
+    }
+    
+    return good;
+};
