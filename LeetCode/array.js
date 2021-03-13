71a67f6046637c3da5c3167e402bef2fa03ee32b
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-13 14:55:19 -0500

    solved

--- LeetCode/array.js
+++ LeetCode/array.js
@@ -162,4 +162,20 @@ Given the array nums, for each nums[i] find out how many numbers in the array ar
 
 Return the answer in an array.
 */
-
+var smallerNumbersThanCurrent = function(nums) {
+    let min = [];
+    let count = 0;
+    
+    for(let i = 0; i < nums.length; i++) {
+        
+        for(let j = 0; j < nums.length; j++) {
+            if(nums[j] < nums[i]) {
+                count += 1;
+            }
+        }
+        min.push(count);
+        count = 0;
+    }
+    
+    return min;
+};
