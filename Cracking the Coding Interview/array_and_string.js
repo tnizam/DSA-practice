05d8aa9359e5a84054a7db0960f9932152e705cb
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-14 21:14:52 -0400

    array prob

--- Cracking the Coding Interview/array_and_string.js
+++ Cracking the Coding Interview/array_and_string.js
@@ -237,3 +237,14 @@ function oneAway(str1, str2) {
     }
     return true;
 }
+
+/*
+1313. Decompress Run-Length Encoded List
+
+We are given a list nums of integers representing a list compressed with run-length encoding.
+
+Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+
+Return the decompressed list.
+*/
+
