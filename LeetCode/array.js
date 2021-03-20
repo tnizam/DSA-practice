aae4ba0fae8a330c454a31024025558eba70cb9f
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-20 16:26:12 -0400

    fib solved

--- LeetCode/array.js
+++ LeetCode/array.js
@@ -295,3 +295,22 @@ var commonChars = function(A) {
     return dups;
 };
 
+/*
+509. Fibonacci Number
+
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+*/
+
+var fib = function(n) {
+    let map = {0:0, 1:1};
+    
+    for(let i = 2; i <= n; i++) {
+        map[i] = map[i - 1] + map[i - 2];
+    }
+    
+    return map[n]
+};
