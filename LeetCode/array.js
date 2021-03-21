d91d4d7edc8de07b652408751a9a56f730606f80
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-20 20:52:27 -0400

    1200 solved

--- LeetCode/array.js
+++ LeetCode/array.js
@@ -314,3 +314,41 @@ var fib = function(n) {
     
     return map[n]
 };
+
+/*
+1200. Minimum Absolute Difference
+
+Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements. 
+
+Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+
+a, b are from arr
+a < b
+b - a equals to the minimum absolute difference of any two elements in arr
+*/
+
+var minimumAbsDifference = function(arr) {
+    let min = [];
+    let minDiff = null;
+    arr.sort((a, b) => a-b);
+    
+    for(let i = 0; i < arr.length; i++) {
+        let diff = Math.abs(arr[i] - arr[i + 1]);
+        if(minDiff === null || diff < minDiff) {
+            minDiff = diff;
+        }
+    }
+    
+    // since it is sorted in order so dont have to loop through it twice
+    // and the integers in the arr are all distinct!!
+    
+    for(let i = 0; i < arr.length; i++) {
+        let diff = Math.abs(arr[i] - arr[i + 1]);
+        
+        if(diff === minDiff) {
+            min.push([arr[i], arr[i + 1]]);
+        }
+    }
+    
+    return min;
+};
