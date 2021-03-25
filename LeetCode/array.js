d364e8890c8cdb1610caa7513f4af0bbfa29bfff
Tahmina Nizam <tahmina.nizam94@gmail.com>
2021-03-25 18:02:49 -0400

    896 solved

--- LeetCode/array.js
+++ LeetCode/array.js
@@ -510,3 +510,20 @@ var moveZeroes = function(nums) {
     
     return nums;
 };
+
+// 896. Monotonic Array
+
+var isMonotonic = function(A) {
+    let increase = true;
+    let decrease = true;
+    
+    for(let i = 0; i < A.length - 1; i++) {
+        if(A[i] < A[i + 1]) {
+            increase = false;
+        } else if(A[i] > A[i + 1]) {
+            decrease = false;
+        }
+    }
+    
+    return increase || decrease;
+};
